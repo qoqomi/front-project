e7a2da2ac8b4040d0213ce306b91fcc9816bb9b4
qoqomi <energyy044@gmail.com>
2022-06-10 15:42:23 +0900

    add file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import Login from "./pages/Login";
+import { Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
+import Login from "./pages/Login";
 import Sign from "./pages/Sign";
 import Write from "./pages/Write";
 
 function App() {
   return (
     <div className="App">
-      <>페이지</>
+      <Routes>
+        <Route path="/" exact element={<Main />}></Route>
+        <Route path="/login" exact element={<Login />}></Route>
+        <Route path="/Sign" exact element={<Sign />}></Route>
+        <Route path="/Write" exact element={<Write />}></Route>
+      </Routes>
     </div>
   );
 }
