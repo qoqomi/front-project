637036efd23203270db7b544d56b58a8c9104762
puppyGugu <lbs7230@naver.com>
2022-06-10 17:42:53 +0900

    sign 컴포넌트 이름 변경

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
 import Login from "./pages/Login";
-import Sign from "./pages/Sign";
+import Signup from "./pages/Signup";
 import Write from "./pages/Write";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
+      <Nav/>
       <Routes>
         <Route path="/" exact element={<Main />}></Route>
         <Route path="/login" exact element={<Login />}></Route>
-        <Route path="/Sign" exact element={<Sign />}></Route>
+        <Route path="/Signup" exact element={<Signup />}></Route>
         <Route path="/Write" exact element={<Write />}></Route>
       </Routes>
     </div>
