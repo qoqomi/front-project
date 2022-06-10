3692919c7afc2ed145998bb91a27df8772d6139c
puppyGugu <lbs7230@naver.com>
2022-06-10 18:20:55 +0900

    작성 컴포넌트 style 작업

--- src/pages/Write.js
+++ src/pages/Write.js
@@ -1,11 +1,66 @@
 import React from "react";
+import styled from "styled-components";
 
 const Write = () => {
   return (
-    <>
-      <div>수정 페이지</div>
-    </>
+    <Wrap>
+      <H4>TIL 작성</H4>
+      <Input type="text" placeholder="제목" />
+      <Textarea placeholder="내용을 입력해주세요"></Textarea>
+      <Label>대표 이미지 선택</Label>
+      <Input type="file" placeholder="제목" />
+      <Button>등록</Button>
+    </Wrap>
   );
 };
+
+const Wrap = styled.div`
+width:100%;
+max-width:500px;
+margin:40px auto 0;
+padding:40px 20px;
+border:3px solid #eee;
+border-radius:20px;
+text-align:center;
+background-color:#fff;
+`
+
+const H4 = styled.h4`
+margin-top:0;
+font-size:1.4em;
+`
+
+const Input = styled.input`
+display:block;
+width:100%;
+max-width:200px;
+height:30px;
+margin:20px auto;
+`
+
+const Textarea = styled.textarea`
+display:block;
+width:100%;
+max-width:300px;
+height:200px;
+margin:20px auto;
+`
+
+const Button = styled.button`
+width:100%;
+max-width:210px;
+height:40px;
+margin:20px auto 0;
+border:none;
+border-radius:6px;
+background-color:#000;
+color:#fff;
+cursor:pointer;
+`
+
+const Label = styled.label`
+display:block;
+margin:30px auto 0;
+`
 
 export default Write;
