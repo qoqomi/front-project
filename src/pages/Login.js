ea52ca36584cd38a9e1e5e3dff3c368d95147373
puppyGugu <lbs7230@naver.com>
2022-06-10 16:44:15 +0900

    회원가입, 로그인 컴포넌트 style 적용

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,52 @@
 import React from "react";
+import styled from "styled-components";
 
 const Login = () => {
   return (
-    <>
-      <div>로그인페이지</div>
-    </>
+    <Wrap>
+      <H4>로그인</H4>
+      <Input type="text" placeholder="아이디(email)" />
+      <Input type="password" placeholder="비밀번호(6자리 이상)" />
+      <Button>로그인 하기</Button>
+      <P>회원이 아니시라면?</P>
+      <Button>회원가입 하기</Button>
+    </Wrap>
   );
 };
+
+const Wrap = styled.div`
+width:100%;
+max-width:500px;
+margin:0 auto;
+text-align:center;
+`
+
+const H4 = styled.h4`
+font-size:1.4em;
+`
+
+const Input = styled.input`
+display:block;
+width:100%;
+max-width:200px;
+height:30px;
+margin:20px auto;
+`
+
+const Button = styled.button`
+width:100%;
+max-width:210px;
+height:40px;
+margin:20px auto 0;
+border:none;
+border-radius:6px;
+background-color:#000;
+color:#fff;
+cursor:pointer;
+`
+
+const P = styled.p`
+margin:50px auto 0;
+`
 
 export default Login;
--- src/pages/Sign.js
+++ src/pages/Sign.js
@@ -1,11 +1,48 @@
 import React from "react";
+import styled from "styled-components";
 
 const Sign = () => {
   return (
-    <>
-      <div>회원가입 페이지</div>
-    </>
+    <Wrap>
+      <H4>회원가입</H4>
+      <Input type="text" placeholder="아이디(email)" />
+      <Input type="password" placeholder="비밀번호(6자리 이상)"/>
+      <Input type="password" placeholder="비밀번호 확인"/>
+      {/* <Input type="text" placeholder="닉네임" /> */}
+      <Button>회원가입 하기</Button>
+    </Wrap>
   );
 };
+
+const Wrap = styled.div`
+width:100%;
+max-width:500px;
+margin:0 auto;
+text-align:center;
+`
+
+const H4 = styled.h4`
+font-size:1.4em;
+`
+
+const Input = styled.input`
+display:block;
+width:100%;
+max-width:200px;
+height:30px;
+margin:20px auto;
+`
+
+const Button = styled.button`
+width:100%;
+max-width:210px;
+height:40px;
+margin:20px auto 0;
+border:none;
+border-radius:6px;
+background-color:#000;
+color:#fff;
+cursor:pointer;
+`
 
 export default Sign;
