dc9ad107185b15519a4ffa0fae0ef5515f73af5c
qoqomi <energyy044@gmail.com>
2022-06-12 15:20:34 +0900

    read me 서버와 프론트단 연결 설명

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useEffect } from "react";
+
 import { Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
 import Login from "./pages/Login";
