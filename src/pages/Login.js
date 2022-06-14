c2f07a8174d29f49dddcb572df3fe72c20af74a7
puppyGugu <65080345+puppyGugu@users.noreply.github.com>
2022-06-15 02:20:27 +0900

    로그인시 db에서 토큰 받아오기

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,18 +2,36 @@ import React from "react";
 import styled from "styled-components";
 
 import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
+import { loginFB } from "../redux/modules/user";
 
 const Login = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [id, setId] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const login = () => {
+    dispatch(loginFB(
+      id, password
+    ))
+  }
 
   return (
     <Wrap>
       <H4>로그인</H4>
-      <Input type="text" placeholder="아이디" />
-      <Input type="password" placeholder="비밀번호(6자리 이상)" />
+      <Input type="text" placeholder="아이디" onChange={
+        (e) => setId(e.target.value)
+      } />
+      <Input type="password" placeholder="비밀번호(6자리 이상)" onChange={
+        (e) => setPassword(e.target.value)
+      } />
       <Button
         onClick={() => {
           navigate("../");
+          login();
         }}
       >
         로그인 하기
--- src/redux/modules/user.js
+++ src/redux/modules/user.js
@@ -1,34 +1,12 @@
 import axios from "axios";
-// import { createAction, handleActions } from "redux-actions";
 import { createAction } from "redux-actions";
 
-// import { actionCreators as userActions } from "../redux/modules/user";
-
-// axios.defaults.baseURL = "api/user/signup";
-axios.defaults.baseURL = "http://15.165.160.84/api/user/login";
-axios.defaults.withCredentials = true;
-
-// import { setCookie, deleteCookie } from "./Cookie";
+// axios.defaults.baseURL = "http://15.165.160.84";
+// axios.defaults.withCredentials = true;
 
 // Actions
-// const LOAD = 'post/LOAD';
-// const CREATE = 'post/CREATE';
-// const UPDATE = 'my-app/widgets/UPDATE';
-// const REMOVE = 'my-app/widgets/REMOVE';
 const SET_USER = "SET_USER";
 const LOG_OUT = "LOG_OUT";
-
-// 초기값
-// const initialState = {
-//     list: [
-//         { id: "아이디 1", password: "비밀번호 1", username: "닉네임 1" }
-//     ]
-// }
-
-// const initialState = {
-//     user: null,
-//     is_login: false,
-// }
 
 const initialState = {
     user: {
@@ -38,9 +16,7 @@ const initialState = {
     msg: '',
 };
 
-
 // Action Creators
-
 const setUser = createAction(SET_USER, (user) => ({ user }))
 const logOut = createAction(LOG_OUT, (user) => ({ user }));
 
@@ -48,32 +24,15 @@ const logOut = createAction(LOG_OUT, (user) => ({ user }));
 //     return { type: LOAD };
 // }
 
-// export function loadPost(post_list) {
-//     return { type: LOAD, post_list };
-// }
-
-// export function createPost(post) {
-//     return { type: CREATE, post };
-// }
-
-// export function updateWidget(widget) {
-//     return { type: UPDATE, widget };
-// }
-
-// export function removeWidget(widget) {
-//     return { type: REMOVE, widget };
-// }
-
-
 // middlewares
-export const signupDB = (id, password, nickname) => {
-    return async function (dispatch, getState) {
-        // await axios.post("api/user/signup",
+// 회원가입
+export const signupDB = (username, password) => {
+    // return async function (dispatch, getState) {
+    return async function () {
         await axios.post("http://15.165.160.84/api/user/login",
             {
-                id: id,
+                username: username,
                 password: password
-                // nickname: nickname
             }
         )
             .then((user) => {
@@ -90,75 +49,72 @@ export const signupDB = (id, password, nickname) => {
     }
 }
 
+// 로그인
+export const loginFB = (username, password) => {
+    console.log(username, password)
+    // return function (dispatch, getState, { history }) {
+    return function (dispatch) {
+
+        // axios는 axios.요청타입으로 요청을 보낼 수 있다. 이 방식을 별칭 메서드라고 부른다.
+        // 예시)
+        // axios.get(url, config)
+        // axios.post(url, data, config)
+
+        // 어떤 요청을 보낼지, 별칭 메서드 사용
+        axios.post('/api/user/login',
+            {
+                username: username,
+                password: password
+            },
+            {
+                // headers: { 'Authorization': `Bearer ${localStorage.getItem("token")}` },
+                headers: { 'Authorization': ` ${localStorage.getItem("token")}` },
+            } // 누가 요청했는지 알려준다. (config에서 작동)
+        ).then(function (response) {
+            console.log(response);
+
+            const token = response.data.token
+            localStorage.setItem("token", token)
+
+            // history.push('/');
+
+        })
+        .catch(function (error) {
+            window.alert('아이디 혹은 비밀번호가 일치하지 않습니다.');
+            console.log(error);
+        });
+
+    }
+}
+
 // 로그인 된 상태인지 확인
 export const loginCheckFB = () => {
-    return function (dispatch, getState, { history }) {
-
-        axios.get('/api/user/me',
+    // return function (dispatch, getState, { history }) {
+    return function (dispatch) {
+        // axios.get('/api/user/me',
+        axios.get('/api/user/auth',
             {
                 // headers: { 'Authorization': `Bearer ${localStorage.getItem("token")}` },
                 headers: { 'Authorization': ` ${localStorage.getItem("token")}` },
             }
         )
-
-            .then(function (response) {
-                // console.log("logincheckFB !! ", response);
-
-                if (response.data.user) {
-                    dispatch(setUser({
-                        userID: response.data.user.userID,
-                        id: response.data.user.id,
-                        // nickname: response.data.user.nickname,
-                        token: localStorage.getItem("token"),
-                    }));
-                } else {
-                    dispatch(logOut());
-                }
-            })
-
-            .catch(function (error) {
-                console.log('logincheckFB error !!', error);
-            });
-    }
-}
-
-// 로그인
-export const loginFB = (id, pwd) => {
-    return function (dispatch, getState, { history }) {
-
-        // axios는 axios.요청타입으로 요청을 보낼 수 있어요. 이 방식을 별칭 메서드라고 불러요.
-        // 예시)
-        // axios.get(url, config)
-        // axios.post(url, data, config)
-
-        // 어떤 요청을 보낼 지, 별칭 메서드 사용
-        axios.post('/api/user/login', // 미리 약속한 주소
-            {
-                userID: id,
-                password: pwd,
-            }, // 서버가 필요로 하는 데이터를 넘겨주고,
-            // {
-            //   headers: { 'Authorization': '내 토큰 보내주기' },
-            //   // authorization: `Bearer ${localStorage.getItem("token")}`
-            // } // 누가 요청했는 지 알려줍니다. (config에서 해요!)
-        ).then(function (response) {
-            // console.log('로그인정보확인',response);
-
-            dispatch(setUser({
-                userID: response.data.user.userID,
-                id: response.data.user.id,
-                nickname: response.data.user.nickname,
-                token: response.data.token,
-            }));
-
-            history.push('/');
-
+        .then(function (response) {
+            console.log("logincheckFB !! ", response);
+            if (response.data.user) {
+                dispatch(setUser({
+                    userID: response.data.user.userID,
+                    id: response.data.user.id,
+                    // nickname: response.data.user.nickname,
+                    token: localStorage.getItem("token"),
+                }));
+            } else {
+                dispatch(logOut());
+            }
         })
-            .catch(function (error) {
-                window.alert('아이디 혹은 비밀번호가 일치하지 않습니다.');
-                console.log(error);
-            });
 
+        .catch(function (error) {
+            console.log('logincheckFB error !!', error);
+        });
     }
 }
 
