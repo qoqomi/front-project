250f36b750cebd660af18e9489dfc1ac8495021c
qoqomi <energyy044@gmail.com>
2022-06-16 13:15:50 +0900

    회원가입 기능 추가 / 서버 루트 추가

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -27,6 +27,12 @@ module.exports = function (app) {
     })
   );
   app.use(
+    createProxyMiddleware("/api/user/info", {
+      target: "http://15.165.160.84", //접속하려는 서버의 루트 URL
+      changeOrigin: true,
+    })
+  );
+  app.use(
     createProxyMiddleware("/api/user/signup", {
       target: "http://15.165.160.84", //접속하려는 서버의 루트 URL
       changeOrigin: true,
